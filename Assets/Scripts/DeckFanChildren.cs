adb02a1956813e58ccc209a966f4c11e2a678a08
Babar Iqbal <bi38@student.london.ac.uk>
2025-02-05 20:51:12 +0500

    initial work on card deck UI

---
+++ Assets/Scripts/DeckFanChildren.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckFanChildren : MonoBehaviour {
+    void Start() {
+		StartCoroutine(RotateChildren());
+    }
+
+	IEnumerator RotateChildren() {
+		// wait for canvas to update and layout elements
+		Canvas.ForceUpdateCanvases();
+		yield return new WaitForEndOfFrame();
+		Canvas.ForceUpdateCanvases();
+		
+		RectTransform parentRect = GetComponent<RectTransform>();
+		float maxDist = transform.GetChild(0).gameObject.GetComponent<RectTransform>().position.x - parentRect.position.x;
+		
+		foreach(Transform tr in transform) {
+			RectTransform rectChild = tr.gameObject.GetComponent<RectTransform>();
+			float dist = rectChild.position.x - parentRect.position.x;
+			float rotation = 5.0f + (dist / maxDist) * 5.0f;
+			rectChild.transform.eulerAngles = new Vector3(0.0f, 0.0f, rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/UIBringToFront.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBringToFront : MonoBehaviour {
+	public GameObject target;
+	private int originalPos;
+	
+	private GameObject empty;
+	
+	void Start() {
+        originalPos = target.transform.GetSiblingIndex();
+		
+		empty = new GameObject();
+		
+		// add rect to ghost object
+		RectTransform rect = empty.AddComponent(typeof(RectTransform)) as RectTransform;
+		RectTransform targetRect = target.GetComponent<RectTransform>();
+		rect.anchorMin = targetRect.anchorMin;
+		rect.anchorMax = targetRect.anchorMax;
+		rect.anchoredPosition = targetRect.anchoredPosition;
+		rect.sizeDelta = targetRect.sizeDelta;
+		
+		// disable the ghost object
+		empty.SetActive(false);
+		empty.transform.SetParent(target.transform.parent);
+    }
+
+	void BringToFront() {
+		target.GetComponent<LayoutElement>().ignoreLayout = true;
+		target.transform.SetAsLastSibling();
+
+		// replace with empty object
+		empty.SetActive(true);
+		empty.transform.SetSiblingIndex(originalPos);
+	}
+
+	void RestorePosition() {
+		target.GetComponent<LayoutElement>().ignoreLayout = false;
+		target.transform.SetSiblingIndex(originalPos);
+
+		empty.SetActive(false);
+	}
+}
