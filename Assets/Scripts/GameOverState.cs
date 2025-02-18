6aab205acd32e592f2c16e7a3187212b512cc539
Lucas Morihiro Morigaki Tanaka <tanaka.morihiro@gmail.com>
2025-02-18 13:14:55 +0900

    Setup UI scenes
    Setup the basic UI placement for the elements in the menu, game board, and shop

---
+++ Assets/Scripts/GameOverState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverState : GameState
+{
+    public override void EnterState(GameStateManager state)
+    {
+        base.EnterState(state);
+        Debug.Log("Gameover triggered");
+    }
+}
---
+++ Assets/Scripts/ShopState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+
+public class ShopState : GameState
+{
+    public override void EnterState(GameStateManager state)
+    {
+        base.EnterState(state);
+        SceneManager.LoadScene("Shop");
+    }
+}
