f181bd70bf4c97b31cd9c409bb689e038a0b084c
Lucas Morihiro Morigaki Tanaka <tanaka.morihiro@gmail.com>
2025-03-04 17:00:28 +0900

    Add UI
    Added UI to gameboard and main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject StartButton;
+
+    [SerializeField]
+    private GameObject QuitButton;
+
+    public void OnStartClicked()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Gameboard");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+}
